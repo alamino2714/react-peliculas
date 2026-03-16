77486110dc4b9eb824c4b58bdf33b3b099683107
alamino2714 <alamino2714@gmail.com>
2026-03-16 12:44:44 -0300

    Actualizo componente de Editar Generos y creo un Formulario de Genero

--- src/features/generos/componentes/CrearGeneros.tsx
+++ src/features/generos/componentes/CrearGeneros.tsx
@@ -1,20 +1,12 @@
-import { NavLink } from "react-router";
-import Boton from "../../../componentes/Boton";
-import { useForm, type SubmitHandler } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { primeraLetraMayuscula } from "../../../validaciones/Validaciones";
-export default function CrearGenero() {
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors ,isValid, isSubmitting}
-    } = useForm<FormType>({ 
-             resolver: yupResolver(reglasDeValidacion) ,
-             mode:'onChange'
-        });
-    const onSubmit: SubmitHandler<FormType> = async (data) => {
+import { type SubmitHandler } from "react-hook-form";
+import type GeneroCreacion from "./modelos/GeneroCreacion.models";
+import FormularioGenero from "./FormularioGenero";
+
+
+export default function CrearGenero() {
+    const onSubmit: SubmitHandler<GeneroCreacion> = async (data) => {
+           console.log('Creando el genero');
         await new Promise(resolve => setTimeout(resolve,2000));
         console.log(data);
     };
@@ -22,26 +14,9 @@ export default function CrearGenero() {
     return (
         <>
             <h3>Crear Genero</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-group">
-                    <label htmlFor="nombre">Nombre:</label>
-                    <input autoComplete="off" className="form-control" {...register('nombre')} />
-                          {errors.nombre && <p className="error">{errors.nombre.message}</p>}
-                </div>
-
-                <div className="mt-2">
-                    <Boton type="submit" disabled={!isValid || isSubmitting}>{isSubmitting?'Enviando...':'Enviar'}</Boton>
-                    <NavLink to="/generos" className="btn btn-secondary ms-2">Cancelar</NavLink>
-                </div>
-            </form>
+            <FormularioGenero onSubmit={onSubmit}/>            
         </>
     )
 }
 
-interface FormType {
-    nombre: string;
-}
-
-const reglasDeValidacion = yup.object({
-    nombre: yup.string().required('El nombre es requerido').test(primeraLetraMayuscula())
-});
+ 
--- src/features/generos/componentes/EditarGenero.tsx
+++ src/features/generos/componentes/EditarGenero.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import type GeneroCreacion from "./modelos/GeneroCreacion.models";
+import FormularioGenero from "./FormularioGenero";
+import type { SubmitHandler } from "react-hook-form";
+import Cargando from "../../../componentes/Cargando";
 
-export default function EditarGenero(){
+export default function EditarGenero() {
     const { id } = useParams();
+    const [modeloGenero, setModeloGenero] = useState<GeneroCreacion | undefined>(undefined)
+
+    useEffect(
+        () => {
+            const timerId = setTimeout(() => {
+                setModeloGenero({ nombre: 'Drama' + id })
+            }, 1000);
+
+            return () => clearTimeout(timerId);
+        }, [id])
+
+    const onSubmit: SubmitHandler<GeneroCreacion> = async (data) => {
+        console.log('Editando el genero');
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log(data);
+    };
+
+
     return (
         <>
-        <h3>Editar Genero</h3>
-        <p> El id es: {id}</p>
+            <h3>Editar Genero</h3>
+            {modeloGenero ?<FormularioGenero modelo={modeloGenero} onSubmit={onSubmit} /> : <Cargando/>}
         </>
     )
 
